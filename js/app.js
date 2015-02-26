6a2b1d8cf2834a3f243d7525a41405c166b52355
Sarrah Vesselov <sarchka1@gmail.com>
2015-02-26 16:26:03 -0500

    added app, api, and main files

---
+++ js/app.js
@@ -0,0 +1,39 @@
+var App = function() {
+
+  function App(data) {
+
+    this.data = data;
+
+    this.$profile = $(".profile");
+    this.$repo = $(".repo-content");
+
+  }
+
+
+  /*will need:
+
+  var orgs
+  var starred
+  var user
+  var repo
+
+
+  showProfile (){
+  
+  }
+
+  showrepos (){
+  
+  }
+
+  showProfile (){
+  
+  }
+
+  spit out all the stuff
+
+
+  */
+  return App;
+
+})();
--- js/templates.js
+++ js/templates.js
@@ -6,17 +6,6 @@ this["JST"]["icon"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"mai
     + "\"></span>";
 },"useData":true});
 this["JST"] = this["JST"] || {};
-this["JST"]["repo"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
-  return "<div class=\"repo\">\n  <div class=\"icon\">\n    <span class=\"octicon octicon-repo\"></span>\n  </div>\n  <div class=\"details\">\n    <div class=\"name\">"
-    + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
-    + "</div>\n    <div class=\"desc\">"
-    + escapeExpression(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"description","hash":{},"data":data}) : helper)))
-    + "</div>\n  </div>\n  <div class=\"stars\">\n    <span class=\"stars-count\">"
-    + escapeExpression(((helper = (helper = helpers.stargazers_count || (depth0 != null ? depth0.stargazers_count : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"stargazers_count","hash":{},"data":data}) : helper)))
-    + "</span>\n    <span class=\"octicon octicon-star\"></span>\n  </div>\n</div>\n";
-},"useData":true});
-this["JST"] = this["JST"] || {};
 this["JST"]["repoTab"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
   var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
   return " <div class=\"repo-tab\">\n   <div class=\"repo-tab-left\">\n    <h3>"
