9090d216d77bb5588fadf6e63034d6c474a32b3a
Sarrah Vesselov <sarchka1@gmail.com>
2015-02-26 15:16:39 -0500

    Implemented and tested ajax pull of api info

--- js/templates.js
+++ js/templates.js
@@ -16,3 +16,20 @@ this["JST"]["repo"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"mai
     + escapeExpression(((helper = (helper = helpers.stargazers_count || (depth0 != null ? depth0.stargazers_count : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"stargazers_count","hash":{},"data":data}) : helper)))
     + "</span>\n    <span class=\"octicon octicon-star\"></span>\n  </div>\n</div>\n";
 },"useData":true});
+this["JST"] = this["JST"] || {};
+this["JST"]["repoTab"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+  return " <div class=\"repo-tab\">\n   <div class=\"repo-tab-left\">\n    <h3>"
+    + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
+    + "</h3>\n    <h4>"
+    + escapeExpression(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"description","hash":{},"data":data}) : helper)))
+    + "</h4>\n    <h5>"
+    + escapeExpression(((helper = (helper = helpers.updated_at || (depth0 != null ? depth0.updated_at : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"updated_at","hash":{},"data":data}) : helper)))
+    + "</h5>\n  </div>\n  <div class=\"repo-tab-right\">\n  <ul class=\"repo-icons\">\n    <li>"
+    + escapeExpression(((helper = (helper = helpers.language || (depth0 != null ? depth0.language : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"language","hash":{},"data":data}) : helper)))
+    + "</li>\n    <li><span class=\"octicon octicon-star\"></span>"
+    + escapeExpression(((helper = (helper = helpers.stargazers_count || (depth0 != null ? depth0.stargazers_count : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"stargazers_count","hash":{},"data":data}) : helper)))
+    + "</li>\n    <li><span class=\"octicon octicon-git-branch\"></span>"
+    + escapeExpression(((helper = (helper = helpers.forks_count || (depth0 != null ? depth0.forks_count : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"forks_count","hash":{},"data":data}) : helper)))
+    + "</li>\n  </ul>\n  </div>\n</div>";
+},"useData":true});
