5e16d6b72042d0ec48402326fcee8e7867d30366
Sarrah Vesselov <sarchka1@gmail.com>
2015-02-27 08:24:14 -0500

    created template files for user section

--- js/templates.js
+++ js/templates.js
@@ -6,6 +6,10 @@ this["JST"]["icon"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"mai
     + "\"></span>";
 },"useData":true});
 this["JST"] = this["JST"] || {};
+this["JST"]["orgs"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  return "<div class=\"orgs\">\n  <h2>Organizations</h2>\n  <span><img src=\"images/org.png\" alt=\"\"></span>\n</div>";
+  },"useData":true});
+this["JST"] = this["JST"] || {};
 this["JST"]["repoTab"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
   var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
   return " <div class=\"repo-tab\">\n   <div class=\"repo-tab-left\">\n    <h3>"
@@ -22,3 +26,11 @@ this["JST"]["repoTab"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"
     + escapeExpression(((helper = (helper = helpers.forks_count || (depth0 != null ? depth0.forks_count : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"forks_count","hash":{},"data":data}) : helper)))
     + "</li>\n  </ul>\n  </div>\n</div>";
 },"useData":true});
+this["JST"] = this["JST"] || {};
+this["JST"]["stats"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  return "<div class=\"stats\">\n  <ul>\n    <li><span>0</span><p>Followers</p></li>\n    <li>\n      <span>1</span>\n      <p>Starred</p>\n    </li>\n    <li>\n      <span>0</span>\n      <p>Following</p>\n    </li>\n  </ul>\n</div>";
+  },"useData":true});
+this["JST"] = this["JST"] || {};
+this["JST"]["user"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  return "<div class=\"user\">\n  <span><img src=\"images/sarrah.jpg\" alt=\"\"></span>\n  <h1>Sarrah Vesselov</h1>\n  <h3>SarrahV</h3>\n</div>\n<div class=\"user-details\">\n  <ul>\n    <li><span class=\"octicon octicon-location\"></span> Tampa Bay</li>\n    <li><span class=\"octicon octicon-clock\"></span> Joined on Sep 10, 2014</li>\n  </ul>\n</div>\n\n";
+  },"useData":true});
